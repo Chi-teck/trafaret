e5e9de97f71ac53cfa699ddb7abd19cd6cf9f5e6
Chi-teck <Chi-teck@users.noreply.github.com>
2020-09-08 03:04:07 +0000

    Add URL validator

---
+++ src/Validator/Url.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Trafaret\Validator;
+
+use Trafaret\Exception\UnexpectedValueException;
+use Trafaret\Exception\UnsupportedConstraintException;
+
+final class Url implements ConstraintValidatorInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function validate(string $constraint, string $value): void
+    {
+        if ($constraint != 'url') {
+            throw new UnsupportedConstraintException();
+        }
+        if (!\filter_var($value, \FILTER_VALIDATE_URL) !== false) {
+            $message = \sprintf('The value "%s" is not a valid URL.', $value);
+            throw new UnexpectedValueException($message);
+        }
+    }
+}
